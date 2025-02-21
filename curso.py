9d497b9b77049a608d6da32c7d86a0722c2f4b9a
Carlos Peñalver Mora <cpm162@alu.ua.es>
2025-02-21 10:03:05 +0100

    Curso1

--- curso.py
+++ curso.py
@@ -6,3 +6,23 @@ class Curso:
         self.codigo = codigo
         self.descripcion = descripcion
         Curso.total_cursos += 1  # Aumenta el contador cada vez que se crea un curso
+
+    def mostrar_detalles(self):
+        print(f'El nombre del curso es {self.nombre}')
+        print(f'La código del curso es {self.codigo}')
+        print(f'Descripción: {self.descripcion}')
+
+
+    def actualizar_descripcion(self, nueva_descripcion):
+        self.nueva_descripcion = nueva_descripcion
+
+    def __str__(self):
+        return f'{self.nombre}, {self.codigo}, {self.descripcion}
+
+    def __repr__(self):
+
+    @classmethod
+    def desde_tupla(cls, tupla):
+
+    @staticmethod
+    def es_curso_abierto():
