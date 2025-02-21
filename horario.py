b21bac4be18c59e18b3b64bb414bdc12f8e73e2e
tomasMarzullo <tm78@alu.ua.es>
2025-02-21 10:00:02 +0100

    Creacion de nuevos metodos
    Se crearon 6 metodos en la rama horario

--- horario.py
+++ horario.py
@@ -7,3 +7,26 @@ class Horario:
         self.hora_inicio = hora_inicio
         self.hora_fin = hora_fin
         Horario.total_horarios += 1  # Aumenta el contador cada vez que se crea un horario
+
+    def mostrar_horarios (self, dia, hora_inicio, hora_fin):
+        print (f'Dia: {self.dia}, Horario de inicio: {self.hora_inicio}, Horario fin: {self.hora_fin}')
+
+
+    def modificar_horario (self, dia, hora_inicio, hora_fin):
+
+
+    def __str__(self):
+        return f"{self.curso}, {self.dia} / {self.hora_inicio}, {self.hora_fin}"
+
+    def __repr__(self):
+        return (
+            f"Curso: {self.curso}"
+            f"Dia: {self.dia}"
+            f"Hora de inicio: {self.hora_inicio}"
+            f"HOra fin: {self.hora_fin}"
+        )
+
+    def desde_tupla ():
+
+
+    def es_horario_valido ():
